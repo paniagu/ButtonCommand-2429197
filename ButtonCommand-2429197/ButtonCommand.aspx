7e358242c84a96876bb0174923c35a58ddb108cc
Paniagu <paniaguadavid084@gmail.com>
2025-01-31 20:29:11 -0600

    Agregar archivos de proyecto.

---
+++ ButtonCommand-2429197/ButtonCommand.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ButtonCommand.aspx.cs" Inherits="ButtonCommand_2429197.ButtonCommand" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Font-Bold="true" Text="View Supplier"></asp:Label>
+            <br /><br />
+            <asp:Button runat="server" CommandArgument="Adidas" CommandName="View" OnCommand="Button_Command" Text="Adidas"/>
+            <asp:Button runat="server" CommandArgument="Babolat" CommandName="View" OnCommand="Button_Command" Text="Babolat"/>
+            <asp:Button runat="server" CommandArgument="Head" CommandName="View" OnCommand="Button_Command" Text="Head" />
+            <asp:Button runat="server" CommandArgument="Nike" CommandName="View" OnCommand="Button_Command" Text="Nike" />
+            <asp:Button runat="server" CommandArgument="Prince" CommandName="View" OnCommand="Button_Command" Text="Prince" />
+            <asp:Button runat="server" CommandName="Cancel" OnCommand="Button_Command" Text="Cancel" />
+            <br /><br />
+            <asp:Label runat="server" ID="lblMessage"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ButtonCommand-2429197/ButtonCommand.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ButtonCommand_2429197
+{
+    public partial class ButtonCommand : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Command(object sender, CommandEventArgs e)
+        {
+            if (e.CommandName == "View")
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                switch (e.CommandArgument.ToString())
+                {
+                    case "Adidas":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Babolat":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Head":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Nike":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Prince":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                }
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Cancelled";
+            }
+
+        }
+
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
